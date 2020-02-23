259d05743633825c775c2a4307020be5f754d597
Andreas Falk <andreas.falk@novatec-gmbh.de>
2020-02-24 00:00:25 +0100

    remove tests in labs 1 - 4

---
+++ lab1/library-server-complete/src/test/java/com/example/libraryserver/LibraryApplicationIntegrationTest.java
@@ -0,0 +1,11 @@
+package com.example.libraryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class LibraryApplicationIntegrationTest {
+
+  @Test
+  void verifyContextLoad() {}
+}
